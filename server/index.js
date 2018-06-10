a2efec0b8821a87002af13436d0e001fdaaefffc
Albert Lee <albert.lee@vanderbilt.edu>
2018-06-10 01:18:34 -0700

    Restoring example code in index.js

--- server/index.js
+++ server/index.js
@@ -5,26 +5,19 @@ const Graph = require('./graph.js');
 const Network = new Graph();
 
 Network.addPage('apple.com');
-Network.addPage('apple.com');
-Network.addPage('apple.com');
-Network.addPage('apple.com');
-Network.addPage('apple.com');
-Network.addPage('apple.com');
-console.log(Network.AdjList)
-// Network.addPage('apple.com');
-// Network.addPage('orange.com');
-// Network.addPage('banana.com');
-// Network.addPage('kiwi.com');
-// Network.addPage('grape.com');
-// Network.createLink('apple.com', 'orange.com');
-// Network.createLink('apple.com', 'kiwi.com');
-// Network.createLink('apple.com', 'grape.com');
-// Network.createLink('orange.com', 'grape.com');
-// Network.createLink('banana.com', 'apple.com');
-// Network.createLink('banana.com', 'orange.com');
-// Network.createLink('kiwi.com', 'orange.com');
-// Network.createLink('grape.com', 'orange.com');
-// Network.calculatePageRank((sortedPages, iterations, err) => {
-//   console.log(sortedPages);
-//   console.log(`Calculated in ${iterations} iterations`);
-// });
+Network.addPage('orange.com');
+Network.addPage('banana.com');
+Network.addPage('kiwi.com');
+Network.addPage('grape.com');
+Network.createLink('apple.com', 'orange.com');
+Network.createLink('apple.com', 'kiwi.com');
+Network.createLink('apple.com', 'grape.com');
+Network.createLink('orange.com', 'grape.com');
+Network.createLink('banana.com', 'apple.com');
+Network.createLink('banana.com', 'orange.com');
+Network.createLink('kiwi.com', 'orange.com');
+Network.createLink('grape.com', 'orange.com');
+Network.calculatePageRank((sortedPages, iterations, err) => {
+  console.log(sortedPages);
+  console.log(`Calculated in ${iterations} iterations`);
+});
